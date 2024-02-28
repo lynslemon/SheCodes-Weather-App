a4ac1d77d331a6b89a829ad5af31f583201294ef
lynslemon <160414828+lynslemon@users.noreply.github.com>
2024-02-27 19:46:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(city) {
   let apiKey = "d23dot43ecc9cadd4fe987930b7e4cb5";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
