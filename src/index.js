99b89a196deb54f6499a39863ada9c147095e986
lynslemon <160414828+lynslemon@users.noreply.github.com>
2024-02-27 19:35:30 -0500

    added api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "d23dot43ecc9cadd4fe987930b7e4cb5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
