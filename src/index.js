21a1dbe12b72f62333317e013cc6434531ee844c
lynslemon <160414828+lynslemon@users.noreply.github.com>
2024-02-29 13:42:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new date(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(temperature);
